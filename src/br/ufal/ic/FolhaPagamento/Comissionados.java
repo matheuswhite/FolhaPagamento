571a43f4204936d9e332e8b3ab7c5ca4575f2baf
Matheus Tenorio <tenoriomatheus@live.com>
2014-09-11 20:12:13 -0300

    add classe Comissionado

---
+++ src/br/ufal/ic/FolhaPagamento/Comissionados.java
@@ -0,0 +1,11 @@
+package br.ufal.ic.FolhaPagamento;
+
+public class Comissionados extends Assalariado {
+
+	public Comissionados(String nome, String endereco, int id,
+			double SalarioFixo) {
+		super(nome, endereco, id, SalarioFixo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
