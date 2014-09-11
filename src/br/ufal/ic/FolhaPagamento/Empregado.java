b41bee1f2bd412814309e98b7a6110a621cd3af3
Matheus Tenorio <tenoriomatheus@live.com>
2014-09-10 21:16:29 -0300

    preenchendo Empregado

--- src/br/ufal/ic/FolhaPagamento/Empregado.java
+++ src/br/ufal/ic/FolhaPagamento/Empregado.java
@@ -1,5 +1,13 @@
 package br.ufal.ic.FolhaPagamento;
 
 public class Empregado {
-
+	private int id;
+	private String nome;
+	private String endereco;
+	private double salarioBruto;
+	private double salarioLiquido;
+	
+	public Empregado() {
+		
+	}
 }
