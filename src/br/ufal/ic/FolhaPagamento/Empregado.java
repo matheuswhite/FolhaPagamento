944e688858b1510f4b5e98253a82e3db3b4c9c79
Matheus Tenorio <tenoriomatheus@live.com>
2014-09-23 12:10:52 -0300

    atualizações da classe Empregado

--- src/br/ufal/ic/FolhaPagamento/Empregado.java
+++ src/br/ufal/ic/FolhaPagamento/Empregado.java
@@ -1,9 +1,9 @@
 package br.ufal.ic.FolhaPagamento;
 
 import java.text.SimpleDateFormat;
-
-
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,18 +11,19 @@ public class Empregado {
 	private int id;
 	private String nome;
 	private String endereco;
+	private String metodoPagamento;
+	
 	private boolean sindicato;
 	private int matriculaSindicato;
-	private String metodoPagamento;
 	
 	protected double salarioBruto;
 	protected double salarioLiquido;
 	protected double salarioProximoMes;
 	protected boolean debitoProximoMes = false;
 	
-	protected Date pontoInicio;
-	protected Date pontoFim;
+	protected int pontos[];
 	
+	//verificar o for
 	public Empregado(String nome, String endereco, int id) {
 		this.id = id;
 		this.nome = nome;
@@ -30,32 +31,49 @@ public class Empregado {
 		this.salarioBruto = 0.0;
 		this.salarioLiquido = 0.0;
 		
-		this.pontoInicio = new Date();
-		this.pontoFim = new Date();
-		
-		this.salarioProximoMes = 0;
+		this.salarioProximoMes = 0.0;
 		
 		this.sindicato = false;
-	} 
-	
-	public void setSalarioProximoMes(double valor, boolean mais) {
-		if(mais){
-			this.salarioProximoMes += valor;
-		}
-		else {
-			this.salarioProximoMes -= valor;
+		
+		this.pontos = new int[31];
+		
+		for(int i : this.pontos) {
+			this.pontos[i] = -1;
 		}
 		
+	} 
+	
+	
+	
+	
+	/* Gets e Sets */
+	
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
-	public void setDebitoProximoMes(boolean valor) {
-		this.debitoProximoMes = valor;
+	public String getNome() {
+		return this.nome;
 	}
 	
-	protected void calcularSalarioLiquido() {
-		 if(this.debitoProximoMes) {
-			 this.salarioLiquido += this.salarioProximoMes;
-		 }
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public String getEndereco() {
+		return this.endereco;
+	}
+	
+	public void setMetodoPagamento(String metodo) {
+		this.metodoPagamento = metodo;
+	}
+	
+	public String getMetodoPagamento() {
+		return this.metodoPagamento;
+	}
+	
+	public int getId() {
+		return this.id;
 	}
 	
 	public void setSindicato(boolean sin) {
@@ -66,50 +84,62 @@ public class Empregado {
 		return this.sindicato;
 	}
 	
+	public void setDebitoProximoMes(boolean valor) {
+		this.debitoProximoMes = valor;
+	}
+	
 	public int getMatricula() {
 		return this.matriculaSindicato;
 	}
 	
-	
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	
-	public void setMetodoPagamento(String metodo) {
-		this.metodoPagamento = metodo;
-	}
+	/* Gets e Sets */
 	
 	public void AssociarAoSindicato(boolean pertence, int matricula) {
 		this.sindicato = pertence;
 		this.matriculaSindicato = matricula;
 	}
 	
-	
-	
-	public String getNome() {
-		return this.nome;
+	protected void calcularSalarioBruto(double salario) {
+		this.salarioBruto = salario;
 	}
 	
-	public String getEndereco() {
-		return this.endereco;
+	protected void calcularSalarioLiquido(double taxafixa) {
+		this.salarioLiquido += taxafixa + this.salarioBruto;
+		
+		if(this.debitoProximoMes) {
+			this.salarioLiquido += this.salarioProximoMes;
+		}
+	}	
+	
+	// ?!
+	public void setSalarioProximoMes(double valor, boolean mais) {
+		if(mais){
+			this.salarioProximoMes += valor;
+		}
+		else {
+			this.salarioProximoMes -= valor;
+		}
 	}
 	
-	public int getId() {
-		return this.id;
-	}
-	
-	public String getMetodoPagamento() {
-		return this.metodoPagamento;
-	}
 	
 	
+	//verificar o for
 	public void baterPonto(Date dateInicio, Date dateFim) {
-		this.pontoInicio = dateInicio;
-		this.pontoFim = dateFim;
+		GregorianCalendar cal = new GregorianCalendar();
+		GregorianCalendar cal2 = new GregorianCalendar();
+		
+		cal.setTime(dateInicio);
+		cal2.setTime(dateFim);
+		
+		int hours = cal.get(Calendar.HOUR_OF_DAY);
+		int hours2 = cal2.get(Calendar.HOUR_OF_DAY);
+		
+		for(int x : this.pontos) {
+			if(this.pontos[x] == -1) {
+				this.pontos[x] = hours - hours2;
+				return ;
+			}
+		}
+		
 	}
 }
